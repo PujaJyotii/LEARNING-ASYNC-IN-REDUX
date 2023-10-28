c4d06ac90008f00bf785345d00121a374d72314e
PujaJyotii <pujajyoti111>
2023-10-28 15:39:58 +0530

    Cart working using redux

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,25 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
     name:'cart',
     initialState:{
         items:[],
-        totalAmount:0
+        totalQuantity:0
     },
     reducers:{
         addToCart(state,action)  {
             const newitem = action.payload;
             const existingItem = state.items.find(item => item.id === newitem.id);
+            state.totalQuantity++
             if(!existingItem)
             {
-                state.items.push({itemId:newitem.id , price: newitem.price ,quantity:1 ,totalPrice:newitem.price,
+                state.items.push({id:newitem.id , price: newitem.price ,quantity:1 ,totalPrice:newitem.price,
                 name: newitem.title})
             }
             else{
-                existingItem.quantity = existingItem.quantity+1,
+                existingItem.quantity = existingItem.quantity+1;
                 existingItem.totalPrice = existingItem.totalPrice + newitem.price
             }
             
         },
+        removeItemFromCart(state,action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id=== id);
+            state.totalQuantity--;
+            state.changed=true;
+            if(existingItem.quantity === 1)
+            {
+                state.items = state.items.filter(item => item.id !== id)
+            }
+            else{
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price
+            }
+        }
     }
 })
+
+export const cartAction = cartSlice.actions
+
+export default cartSlice
